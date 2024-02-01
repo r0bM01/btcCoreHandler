b5ca4436bd4327fbae3d9cb70dcdb77848bf336e
r0bM01 <r0bM01@pm.me>
2024-02-01 12:18:55 +0000

    test tool fixed

--- lib/shared/test_tool.py
+++ lib/shared/test_tool.py
@@ -12,7 +12,7 @@ with open(BASE.joinpath("cert.rob"), "rb") as F:
 ALPHA = {hashlib.blake2b(chr(n).encode(), digest_size = 2, key = CERT).hexdigest() : chr(n) for n in range(32, 127)}
 
 def decode_with_cert(string):
-    return "".join([string[c:c+4] for c in range(0, len(string), 4)])
+    return "".join([ALPHA[string[c:c+4]] for c in range(0, len(string), 4)])
 
 def make_key(data):
     return hashlib.blake2b(data, digest_size = 8, key = CERT).digest()
@@ -20,27 +20,31 @@ def make_key(data):
 def load_database():
     with open(index, "rb") as F:
         dbTemp = F.read()
-    dbTemp = dbTemp.hex()
-    db = { dbTemp[x:x+16] : dbTemp[x+16:x+24] for x in range(0, len(dbTemp), 24) }
+    #dbTemp = dbTemp.hex()
+    db = { dbTemp[x:x+8] : dbTemp[x+8:x+12] for x in range(0, len(dbTemp), 12) }
     return db
 
 def read_value(filePos):
     with open(addrs, "rb") as F:
-        F.seek(int(filePos, 16))
-        size = F.read(4)
+        F.seek(int(filePos.hex(), 16))
+        size = F.read(2)
+        print(int(size.hex(), 16))
         data = F.read(int(size.hex(), 16))
     return data
 
 def get_value(addr):
     ipKey = make_key(ipaddress.ip_address(addr).packed)
     index = load_database()
+    print("loaded: ", len(index))
     if ipKey in index:
         value = read_value(index[ipKey])
         value = value.split(b"#")
+        print(value)
         result = {'ip': ipaddress.ip_address(value[0]).exploded,
                'country_code2': decode_with_cert(value[1].hex()),
                'country_name': decode_with_cert(value[2].hex()), 
                'isp': decode_with_cert(value[3].hex()) }
+        
     else: 
         result = {"error": "value not found!"}
     return result    
@@ -55,4 +59,9 @@ def main():
         if 'error' in value:
             print(value['error'])
         else:
-            [print(value[p]) for p in value]
+            for key, val in value.items():
+                print(f"{key}: {val}")
+
+
+if __name__ == '__main__':
+    main()
