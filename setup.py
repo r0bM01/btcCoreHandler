a6ae4b44cc74cf7d65dbab2e53af1c5e48136514
r0bM01 <r0bM01@pm.me>
2024-02-09 07:45:52 +0000

    installer

--- setup.py
+++ setup.py
@@ -15,49 +15,83 @@
 #############################################################################
 
 import pathlib, shutil, sys
+import secrets
+import argparse
 
 
 class Base:
     def __init__(self):
+
+        self.base = pathlib.Path('/usr/lib/btcCoreHandler')
+
         self.source = {'lib': pathlib.Path.cwd().joinpath('lib'),
                        'ui': pathlib.Path.cwd().joinpath('ui')}
-        self.base = pathlib.Path('/usr/lib/btcCoreHandler')
-        self.dest = {'lib': self.base.joinpath('lib'),
-                     'ui': self.base.joinpath('ui'),
-                     'data': pathlib.Path.home().joinpath('./btcCoreHandler')}
 
-class Server(Base):
+        self.dest = {'lib': self.base.joinpath('lib'),
+                     'ui': self.base.joinpath('ui')}
+        
+        self.home = pathlib.Path.home()
+        
+    def create_certificate(self):
+        return secrets.token_bytes(64)
+    
+    def import_certificate(self, source):
+        with open(pathlib.Path(source), "rb") as F:
+            data = F.read()
+        return data if len(data) == 64 else False
+
     def install(self):
-        print("Installing server files")
-        # creates library folder 
         if not pathlib.Path.exists(self.base):
             pathlib.Path.mkdir(self.base)
         
         # creates folders
         if not pathlib.Path.exists(self.dest['lib']):
             pathlib.Path.mkdir(self.dest['lib'])
+
+        if not pathlib.Path.exists(self.dest['ui']):
+            pathlib.Path.mkdir(self.dest['ui'])
+
         if not pathlib.Path.exists(self.dest['data']):
             pathlib.Path.mkdir(self.dest['data'])
             pathlib.Path.mkdir(self.dest['data'].joinpath('cert'))
-            pathlib.Path.mkdir(self.dest['data'].joinpath('geoDb'))
-            pathlib.Path.mkdir(self.dest['data'].joinpath('logs'))
-        
-        # creates directory tree
+
         try:
             shutil.copytree(self.source['lib'], self.dest['lib'])
-        except Error as E:
-            print("Error while copying files into the directories!")
+            shutil.copytree(self.source['ui'], self.dest['ui'])
+        except OSError as E:
+            print("Error while copying files into directories!")
             print(E)
-            sys.exit()
-        
+
         print("Files correctly installed!")
+
+        if bool(importCertificate):
+            certificate = self.import_certificate(importCertificate)
+        else:
+            certificate = self.create_certificate()
         
+        if bool(certificate):
+            homeFolder = self.dest['data']
+            with open(self.dest['data'].joinpath('cert', 'cert.r0b'), "wb") as C:
+                C.write(certificate)
+            print("Certificate correctly created/imported!")
+        else:
+            print("Error in creating/importing certificate!")
+        sys.exit()
+
 
         
         
         
         
         
-        
+def main():
+    print("TESTING CLIENT INSTALLER")
+
+    C = Client()
+    C.install()
+
+
+if __name__ == '__main__':
+    main()
 
     
