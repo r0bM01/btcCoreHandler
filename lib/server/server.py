19fd4bf4c79dba8c04abfeb88dfb067810375108
r0bM01 <r0bM01@pm.me>
2024-04-29 08:13:29 +0100

    services added

--- lib/server/server.py
+++ lib/server/server.py
@@ -15,18 +15,87 @@
 
 
 
-import json, time, subprocess, threading, platform, sys
+import json, time, subprocess, threading, platform, sys, signal
 import lib.shared.network
 import lib.shared.crypto
 import lib.server.machine
 import lib.server.protocol
 import lib.server.srpc
 
+class Services:
+    def __init__(self, bitcoin_data, geolocation_data, logger):
 
+        self.services = [{'name': 'bitcoin', 'target': self.bitcoin_service, 'active': False}, 
+                         {'name': 'geodata', 'target': self.geodata_service, 'active': False}]
+
+        self.services_controller = threading.Event()
+
+        self.bitcoin = bitcoin_data
+        self.geolocation = geolocation_data
+        self.logger = logger
+
+        self.worker = self.make_new_thread()
+        self.worker_rest = 30
+
+    def start(self):
+        for service in self.services:
+            service['active'] = True # activates all services
+            self.logger.add("server- service", service['name'], service['active'])
+        if not bool(self.worker):
+            self.worker = self.make_new_thread()
+        if not self.worker.is_alive:
+            self.services_controller.clear()
+            self.worker.start()
+            self.logger.add("server- services working", self.worker.is_alive())
+    
+    def stop(self):
+        for service in self.services:
+            service['active'] = False # deactivates all services
+            self.logger.add("server- service", service['name'], service['active'])
+        if self.worker.is_alive():
+            self.services_controller.set()
+            self.worker.join()
+
+    def make_new_thread(self):
+        return threading.Thread(target = self.services_worker, daemon = True)
+
+    def services_worker(self):
+        while not self.services_controller.is_set():
+            for service in self.services:
+                if service['active']: service['target']() # execute the service if active
+            self.services_controller.wait(self.worker_rest)
+
+    def bitcoin_service(self):
+        uptime = lib.server.machine.MachineInterface.runBitcoindCall("uptime")
+        self.bitcoin.uptime = json.loads(uptime)
+
+        blockchainInfo = lib.server.machine.MachineInterface.runBitcoindCall("getblockchaininfo")
+        self.bitcoin.blockchainInfo = json.loads(blockchainInfo)
+        
+        networkInfo = lib.server.machine.MachineInterface.runBitcoindCall("getnetworkinfo")
+        self.bitcoin.networkInfo = json.loads(networkInfo)
+
+        nettotalsInfo = lib.server.machine.MachineInterface.runBitcoindCall("getnettotals")
+        self.bitcoin.nettotalsInfo = json.loads(nettotalsInfo)
+
+        mempoolInfo = lib.server.machine.MachineInterface.runBitcoindCall("getmempoolinfo")
+        self.bitcoin.mempoolInfo = json.loads(mempoolInfo)
+        
+        miningInfo = lib.server.machine.MachineInterface.runBitcoindCall("getmininginfo")
+        self.bitcoin.miningInfo = json.loads(miningInfo)
+
+        peersInfo = lib.server.machine.MachineInterface.runBitcoindCall("getpeerinfo")
+        self.bitcoin.peersInfo = json.loads(peersInfo)
+
+    def geodata_service(self):
+        self.bitcoin.connectedInfo = self.geolocation.updateDatabase(self.bitcoin.peersInfo, self.logger)
 
 class Server(lib.server.protocol.RequestHandler):
     def __init__(self, logger, storage):
         lib.server.protocol.RequestHandler.__init__(self)
+
+        signal.signal(signal.SIGINT, self.nice_server_shutdown)
+        signal.signal(signal.SIGTERM, self.nice_server_shutdown)
 
         self.initTime = int(time.time())
         self.internetIsOn = lib.shared.network.Utils.checkInternet()
@@ -36,6 +105,7 @@ class Server(lib.server.protocol.RequestHandler):
         self.STORAGE = storage
         self.LOGGER = logger
         self.NETWORK = lib.shared.network.Server(lib.shared.network.Settings(host = lib.server.machine.MachineInterface.getLocalIP()))
+        self.SERVICES = Services(self.BITCOIN_DATA, self.GEO_DATA, self.LOGGER)
 
         self.maxCallSize = 256 #bytes 
 
@@ -54,9 +124,11 @@ class Server(lib.server.protocol.RequestHandler):
         self.isServing = False
         self.isOnline = False
 
+        """
         self.autoCache = threading.Thread(target = self.cacheUpdater, daemon = True)
         self.autoCacheRun = False
         self.autoCacheRest = 30
+        """
         
     def local_server_controller(self):
         # command line operation
@@ -77,7 +149,6 @@ class Server(lib.server.protocol.RequestHandler):
                 elif bool(call) and call == 'handlerstop':
                     self.LOGGER.add("server- received closure call", call)
                     stop = True
-            self.localControllerNetwork.sockClosure() #socket must always be closed
         else:
             self.nice_server_shutdown()
 
@@ -95,35 +166,34 @@ class Server(lib.server.protocol.RequestHandler):
         # self.autoServing.join()
 
         self.LOGGER.add("server- stopping autocache service")
-        self.autoCacheRest = 2 #sets to 2 the sleeping time
-        self.autoCacheRun = False #stops cache updater
+        self.SERVICES.stop()
+        # self.autoCacheRest = 2 #sets to 2 the sleeping time
+        # self.autoCacheRun = False #stops cache updater
         # self.autoCache.join()
 
         self.LOGGER.add("server- shutdown completed")
         self.localControllerNetwork.sender("shutdown completed")
         self.localControllerNetwork.sender("handlerstopped")
+        self.localControllerNetwork.sockClosure() #socket must always be closed
         self.localControllerEvent.set() # this will cause server stop
 
     def start_all(self):
-        self.autoCache.start()
+        # self.autoCache.start()
+        self.SERVICES.start()
         self.autoServing.start()
         self.LOGGER.verbose = False
 
         self.localControllerEvent.wait() # server hangs here!! 
         # when activated it will stop the application
-
         sys.exit(0) 
-        """
-        self.LOGGER.add("server- closure called from thread")
-        self.isServing = False
-        self.autoCacheRun = False
-        """
+
     def start_network(self):
         
         #self.srpcT.start()
         self.NETWORK.openSocket()
         self.isOnline = bool(self.NETWORK.socket)
-        self.LOGGER.add("server-", f"socket ready: {self.isOnline}", f"bind to ip: {self.NETWORK.settings.host}")
+        self.LOGGER.add("server- socket ready", self.isOnline)
+        self.LOGGER.add("server- socket bind to address", self.NETWORK.settings.host)
         #self.LOGGER.add("bind to IP", self.NETWORK.settings.host)
         self.LOGGER.add("server- network succesfully started")
 
