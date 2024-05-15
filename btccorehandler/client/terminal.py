2f9d6ff4c4f843b5942374cf4a666b8b664a8848
r0bM01 <111903628+r0bM01@users.noreply.github.com>
2024-05-15 15:49:35 +0100

    terminal launcher

--- btccorehandler/client/terminal.py
+++ btccorehandler/client/terminal.py
@@ -74,64 +74,8 @@ def main():
 
 
 
-        
 
-
-
-
-
-def clientTerminal():
-    def keepConnAlive():
-        while remoteConn.network.isConnected:
-            eventThread.wait()
-            remoteConn.keepAlive()
-            time.sleep(10)
-        print("\nconnection terminated")
-
-
-    storage = lib.shared.storage.Client()
-
-    certificate = storage.init_certificate()
-
-    print(bool(certificate))
-
-    remoteConn = Client(certificate)
-    eventThread = threading.Event()
-    keepAliveThread = threading.Thread(target = keepConnAlive, daemon = True)
-
-    print("Bitcoin Core Handler terminal")
-    ipAddr = input("insert server ip: \n>> ")
-    input("\nPress enter to connect\n")
-    remoteConn.initConnection(ipAddr)
-    
-    print(f"Handshake code: {remoteConn.network.handshakeCode} \n")
-    print("receiving info from server...")
-    time.sleep(0.5)
-    print(f"connected to server: {remoteConn.network.isConnected}\n")
-
-    if remoteConn.network.isConnected:
-        eventThread.set()
-        keepAliveThread.start()
-        print("keep alive thread started")
-        
-    try:
-
-        while True and remoteConn.network.isConnected:
-            print("\nInsert a call or 'quit' to close the terminal; ")
-            c = str(input("\n>> ")).lower()
-            if c == 'quit': break
-            encodedCall = lib.shared.crypto.getHashedCommand(c, remoteConn.certificate, remoteConn.network.handshakeCode)
-            message = str(remoteConn.calls['advancedcall']) + str(encodedCall)
-            eventThread.clear()
-            if remoteConn.network.sender(message): reply = remoteConn.network.receiver()
-            eventThread.set()
-
-            print(f"\nCall sent: {c}: {encodedCall}")
-            print(f"reply: \n{reply}")
-        remoteConn.closeConnection()
-
-    except KeyboardInterrupt:
-        remoteConn.closeConnection()
-        print("closing")
+if __name__ == '__main__':
+    main()
 
 
